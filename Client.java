d537f4705738ed2c33a85e06d797118f078c199c
Arvin Sarsazi <arvin@Arvins-MacBook-Pro.local>
2025-11-16 18:12:17 -0500

    Client class done

--- Client.java
+++ Client.java
@@ -1,12 +1,18 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
 public class Client {
 
-    private Socket socket;
+    private Socket sock;
     private PrintWriter out;
+    private BufferedReader in;
 
     public Client (String name, int port) throws IOException{
 
-        socket = new Socket(host, port);
-        out = new PrintWriter(socket.getOutputStream());
+        sock = new Socket(name, port);
+        out = new PrintWriter(sock.getOutputStream());
+        in  = new BufferedReader(new InputStreamReader(sock.getInputStream()));
     }
 
     public void handshake () {
@@ -15,14 +21,33 @@ public class Client {
         out.flush();
     }
 
-    public String request(String s) {
+    public String request(String s) throws IOException{
 
         out.println(s);
-        return null;
+        out.flush();
+        return in.readLine();
     }
 
     public Socket getSocket() {
 
-        return socket;
+        return sock;
+    }
+
+    public void disconnect() {
+
+        try {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (sock != null) {
+                sock.close();
+            }
+        }
+        catch (IOException e) {
+
+        }
     }
 }
