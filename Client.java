db6466e17b3b7124b761f790e39edd3c54d4d73d
Arvin Sarsazi <arvin@Arvins-MacBook-Pro.local>
2025-11-16 15:43:47 -0500

    Client methods handshake and request done

--- Client.java
+++ Client.java
@@ -1,18 +1,23 @@
 public class Client {
 
     private Socket socket;
-    
+    private PrintWriter out;
+
     public Client (String name, int port) throws IOException{
 
         socket = new Socket(host, port);
+        out = new PrintWriter(socket.getOutputStream());
     }
 
     public void handshake () {
 
+        out.println("12345");
+        out.flush();
     }
 
     public String request(String s) {
 
+        out.println(s);
         return null;
     }
 
---
+++ Server.java
@@ -0,0 +1,14 @@
+public class Server {
+    
+    public Server(int num) {
+
+    }
+    
+    public void serve(int a) {
+
+    }
+
+    public void disconnect() {
+        
+    }
+}
